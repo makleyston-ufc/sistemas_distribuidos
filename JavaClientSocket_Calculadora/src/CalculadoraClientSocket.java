846fc80b96d120b6262364fe7c622938bc55e887
pedroccufc <pedro_alves96@yahoo.com.br>
2021-07-05 16:34:55 -0300

    sockets

--- JavaClientSocket_Calculadora/src/CalculadoraClientSocket.java
+++ JavaClientSocket_Calculadora/src/CalculadoraClientSocket.java
@@ -4,78 +4,62 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.Scanner;
+import java.awt.event.*;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 
 public class CalculadoraClientSocket {
 
+    public static double oper1;
+    public static double oper2;
+    public static int operacao;
+    public static String result;
+
 	public static void main(String[] args) throws IOException, InterruptedException {
 
-        Scanner ler = new Scanner(System.in);
+        Calculadora calc = new Calculadora();
+        calc.setVisible(true);
 
-        int operacao;
-        double oper1;
-        double oper2;
-        String result;
+        // É criada uma Ação que dispara quando o usuário clica no botão de igual.
+        // Capturando os valores digitados e enviando para o servidor.
+        calc.igual.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                oper1 = calc.getOper1();
+                oper2 = calc.getOper2();
+                operacao = calc.getOperacao();
 
-        while (true) {
-            
-            // TODO Auto-generated method stub
-            // double oper1 = 10, oper2 = 20;
-            // int operacao = 5; //1-somar 2-subtrair 3-dividir 4-multiplicar
+                calc.resetVariaveis();
 
-            // Usando o Scanner para ler os valores do usuário.
+                try {
+                    //Conexão com o Servidor
+                    Socket clientSocket = new Socket("192.168.1.106", 9090);
+                    DataOutputStream socketSaidaServer = new DataOutputStream(clientSocket.getOutputStream());
 
-            System.out.println("1 - Soma");
-            System.out.println("2 - Subtração");
-            System.out.println("3 - Divisão");
-            System.out.println("4 - Multiplicação");
-            System.out.println("5 - Sair\n");
+                    //Enviando os dados
+                    socketSaidaServer.writeBytes(operacao+"\n");
+                    socketSaidaServer.writeBytes(oper1+ "\n");
+                    socketSaidaServer.writeBytes(oper2+ "\n");
+                    socketSaidaServer.flush();
 
-            System.out.println("Digite a Operação Desejada: ");
-            operacao = ler.nextInt();
+                    //Recebendo a resposta
+                    BufferedReader messageFromServer = new BufferedReader
+                            (new InputStreamReader(clientSocket.getInputStream()));
+                    result = messageFromServer.readLine();
 
-            if (operacao == 5) {
-                break;
+                    // Escreve o resultado na interface da calculadora.
+                    calc.setDisplay(result);
+
+                    // Atualiza o valor da leitura para o resultado da operação.
+                    calc.setLeitura(Double.parseDouble(result));
+
+                    // Imprimindo o resultado
+                    System.out.println("resultado = " + result + "\n");
+                    clientSocket.close();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-
-            System.out.println("Digite o primeiro valor:");
-            oper1 = ler.nextDouble();
-
-            System.out.println("Digite o segundo valor:");
-            oper2 = ler.nextDouble();
-
-            try {
-
-                //Conexão com o Servidor
-                Socket clientSocket = new Socket("192.168.1.115", 9090);
-                DataOutputStream socketSaidaServer = new DataOutputStream(clientSocket.getOutputStream());
-                
-                //Enviando os dados
-                socketSaidaServer.writeBytes(operacao+"\n");
-                socketSaidaServer.writeBytes(oper1+ "\n");
-                socketSaidaServer.writeBytes( oper2+ "\n");
-                socketSaidaServer.flush();
-
-                //Recebendo a resposta
-                BufferedReader messageFromServer = new BufferedReader
-                        (new InputStreamReader(clientSocket.getInputStream()));
-                result = messageFromServer.readLine();
-
-                // "Limpando" o console
-                clearScreen();
-                
-                // Imprimindo o resultado
-                System.out.println("resultado = " + result + "\n");
-                clientSocket.close();
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+        });
 	}
-
-    // Método para "limpar" o console (imprime alguma linhas em branco).
-    private static void clearScreen() {  
-        System.out.print("\033[H\033[2J");  
-        System.out.flush();  
-    } 
 }
